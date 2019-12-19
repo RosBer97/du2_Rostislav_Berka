ff6f679202a78274d58b3a0ad13ccbcc982c0596
RosBer97 <rosstaberrka@seznam.cz>
2019-12-19 21:28:27 +0100

    Čištění kódu, opraveny/doplněny některé programátorské poznámky.

--- split.py
+++ split.py
@@ -2,20 +2,22 @@ from quadtree import quadtree, acquire_bounding_points
 import json
 
 # load GeoJson file:
-with open("input_Pan_Kyncl_Zastavky.geojson", "r", encoding = "utf-8") as f:
+with open("input.geojson", "r", encoding = "utf-8") as f:
     data = json.load(f)
 # loading features to list feat
 feat = data["features"]
 feat_pokus = feat[:] # just for testing purposes, for reducing amount of data through slice index,
-# for more comfortable work and visualisation in QGIS during developing
+# for more comfortable work and visualisation in QGIS during developing, it will be deleted in
+# final version
 
 # features counter:
 c_list = [0]
 # acquire bounding points of rectangle/square set up by given points:
 b_points = acquire_bounding_points(feat_pokus)
+# final list with cluster_id
+qtree_result = []
 # calling recurse function
-qtree_result = [] # final list with cluster_id
-#input is list of given features, features counter, axis (0 or 1), list of outputfeatures, and bounding points)
+#input is list of given features, features counter, list of output features, and bounding points)
 quadtree(feat_pokus, c_list, qtree_result, b_points[0], b_points[1], b_points[2], b_points[3])
 
 # build GeoJson:
